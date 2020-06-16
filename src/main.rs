a4e5fe0875b3fecce532f6e806c1168d526899ae
uedayuji <yuji.ueda.0331@gmail.com>
2020-06-16 18:59:25 +0900

    fix some compile error

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 #[macro_use]
 extern crate diesel;
 
-use actix_web::{get, middleware, post, web, App, Error, HttpResponse, HttpServer};
-use diesel::prelude::*;
-use diesel::mysql::MysqlConnection;
+use actix_web::{middleware, App, HttpServer};
 use dotenv::dotenv;
 use std::env;
 
@@ -12,18 +10,12 @@ mod actions;
 mod domain;
 mod schema;
 
-pub fn establish_connection() -> MysqlConnection {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    MysqlConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
-}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info,diesel=debug");
+    env::set_var("RUST_LOG", "actix_web=info,diesel=debug");
     env_logger::init();
-    dotenv::dotenv().ok();
+    dotenv().ok();
 
     let bind = "0.0.0.0:8080";
 
@@ -32,8 +24,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
-            .service(get_user)
-            .service(add_user)
+            .service(routes::get_user)
+            .service(routes::add_user)
     })
     .bind(&bind)?
     .run()
--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,17 @@
-use actix_web::{get, middleware, post, web, App, Error, HttpResponse, HttpServer};
+use actix_web::{get, post, web, Error, HttpResponse};
+use std::env;
+use dotenv::dotenv;
+use diesel::prelude::*;
+use diesel::mysql::MysqlConnection;
 use crate::actions;
 use crate::domain::model::models;
+
+pub fn establish_connection() -> MysqlConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    MysqlConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
 
 #[get("/user/{user_id}")]
 async fn get_user(user_id: web::Path<i32>) -> Result<HttpResponse, Error> {
